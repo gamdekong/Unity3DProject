2124887a27e75fe79c09f1e5e1819036f69f6d6d
민경명 <rulibird@gmail.com>
2016-06-29 14:10:30 +0900

    Script 추가      -플레이어 움직임 스크립트            -플레이어 전진      -타겟팅 시스템 스크립트            -클릭시 에임 위치 설정 구현            -카메라 밖으로 벗어날시 에임삭제 구현
    Prefab 추가
         -행성
         -소행성
         -에임

--- Assets/02_Scripts/TargetingManager.cs
+++ Assets/02_Scripts/TargetingManager.cs
@@ -3,13 +3,62 @@ using System.Collections;
 
 public class TargetingManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public GameObject Aim;
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        if (GameObject.FindGameObjectWithTag("Aim"))
+        {
+            GameObject aim = GameObject.FindGameObjectWithTag("Aim");
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            if (aim.transform.position.z < player.transform.position.z)
+            {
+                Destroy(aim);
+            }
+        }
+
+            if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.layer == 8)
+                {
+                    //Debug.Log("It's Target");
+                    Vector3 targetPos = hit.transform.position;
+                    targetPos.z -= 20.0f;
+                    if (GameObject.FindGameObjectWithTag("Aim"))
+                    {
+                        GameObject.FindGameObjectWithTag("Aim").transform.position = targetPos;
+                    }
+                    else
+                    {
+                        Instantiate(Aim, targetPos, transform.rotation);
+                    }
+                }
+            }
+        }
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Ended)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.gameObject.layer == 8)
+                    {
+                        //Debug.Log("It's Target");
+                    }
+                }
+            }
+        }
+
+        
+    }
 }
