397bc9680c345cc7f5e0b4c3f3116a4bc20b83e7
공운학 <gamdekong@gmail.com>
2016-06-29 17:07:08 +0900

    fire button 추가

--- Assets/02_Scripts/csFireManager.cs
+++ Assets/02_Scripts/csFireManager.cs
@@ -11,41 +11,41 @@ public class csFireManager : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+    public void FireMissile()
+    {
+        if (Target == null)
+            return;
 
+        GameObject missileObj = Instantiate(missile) as GameObject;
+        Vector3 missilePos = Player.transform.position;
+        Quaternion missileAng = Player.transform.rotation;
+        missilePos.x += Random.Range(-5.0f, 5.0f);
+        missileAng.x += 90.0f;
+        missileObj.transform.position = missilePos;
+        missileObj.transform.rotation = missileAng;
 
-        if (Input.GetButtonUp("Jump"))
+        int range = Random.Range(0, 3);
+        switch (range)
         {
-            if (Target == null)
-                return;
-
-            GameObject missileObj = Instantiate(missile) as GameObject;
-            Vector3 missilePos = Player.transform.position;
-            Quaternion missileAng = Player.transform.rotation;
-            missilePos.x += Random.Range(-5.0f, 5.0f);
-            missileAng.x += 90.0f;
-            missileObj.transform.position = missilePos;
-            missileObj.transform.rotation = missileAng;
-            int range = Random.Range(0, 3);
-            switch(range)
-            {
-                case 0:
-                    missileObj.transform.Rotate(Vector3.right, Random.Range(60, 90));
-                    break;
-                case 1:
-                    missileObj.transform.Rotate(Vector3.left, Random.Range(60, 90));
-                    break;
-                case 2:
-                    missileObj.transform.Rotate(Vector3.up, Random.Range(60, 90));
-                    break;
-                case 3:
-                    missileObj.transform.Rotate(Vector3.down, Random.Range(60, 90));
-                    break;
-            }
-            missileObj.GetComponent<csMissile>().target = Target;
+            case 0:
+                missileObj.transform.Rotate(Vector3.right, Random.Range(60, 90));
+                break;
+            case 1:
+                missileObj.transform.Rotate(Vector3.left, Random.Range(60, 90));
+                break;
+            case 2:
+                missileObj.transform.Rotate(Vector3.up, Random.Range(60, 90));
+                break;
+            case 3:
+                missileObj.transform.Rotate(Vector3.down, Random.Range(60, 90));
+                break;
         }
+        missileObj.GetComponent<csMissile>().target = Target;
+    }
+    // Update is called once per frame
+    void Update () {
+
+
 
     }
 }
