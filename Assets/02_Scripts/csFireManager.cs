4543ac98f652212f32585641e81cbb9ed2fe92f3
민경명 <rulibird@gmail.com>
2016-06-29 16:54:23 +0900

    미사일 각도 수정

--- Assets/02_Scripts/csFireManager.cs
+++ Assets/02_Scripts/csFireManager.cs
@@ -28,7 +28,22 @@ public class csFireManager : MonoBehaviour {
             missileAng.x += 90.0f;
             missileObj.transform.position = missilePos;
             missileObj.transform.rotation = missileAng;
-            missileObj.transform.Rotate(Vector3.left, 90);
+            int range = Random.Range(0, 3);
+            switch(range)
+            {
+                case 0:
+                    missileObj.transform.Rotate(Vector3.right, Random.Range(60, 90));
+                    break;
+                case 1:
+                    missileObj.transform.Rotate(Vector3.left, Random.Range(60, 90));
+                    break;
+                case 2:
+                    missileObj.transform.Rotate(Vector3.up, Random.Range(60, 90));
+                    break;
+                case 3:
+                    missileObj.transform.Rotate(Vector3.down, Random.Range(60, 90));
+                    break;
+            }
             missileObj.GetComponent<csMissile>().target = Target;
         }
 
