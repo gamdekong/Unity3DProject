51823538f249a497ae80ebc179c36fac9f6d7e44
민경명 <rulibird@gmail.com>
2016-06-29 11:50:31 +0900

    기본 설정 추가

---
+++ Assets/02_Scripts/csPlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csPlayerMove : MonoBehaviour {
+
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CheckMobile()
+	{
+	}
+
+	void CheckKeyboard()
+	{
+	}
+}
