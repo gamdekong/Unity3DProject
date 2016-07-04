9e3d82012e7c0f381355cbf4b4e8b2fe5cb851b1
민경명 <rulibird@gmail.com>
2016-07-04 10:17:21 +0900

    플레이어 체력 추가 playerHealth : 최대체력 healthDmgDelay : 몇초에 한틱으로 할것인지 healthDmg : 틱당 감소되는 체력량

--- Assets/02_Scripts/csPlayerStatus.cs
+++ Assets/02_Scripts/csPlayerStatus.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class csPlayerStatus : MonoBehaviour {
 
+    public int playerHealth = 100;
+    public float healthDmgDelay = 3.0f;
+    public int healthDmg = 1;
+    int health;
+    int damage;
+    float delay;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        delay = healthDmgDelay;
+        health = playerHealth;
+        damage = healthDmg;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if(delay > 0)
+        {
+            delay -= Time.deltaTime;
+        }
+        else
+        {
+            delay = healthDmgDelay;
+            if (health > 0)
+            {
+                health -= damage;
+                Debug.Log("PlayerHP : " + health);
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
 }
