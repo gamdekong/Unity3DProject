805370f45b4bd3a32368b85a3c585ad970515772
gamdekong <gamdekong@gmail.com>
2016-07-28 01:41:28 +0900

    ui- 스테이지 추가

---
+++ Assets/02_Scripts/UI/Creation/MakingCardScrollControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakingCardScrollControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public void ScrollViewReSet()
+    {
+        GetComponent<UIScrollView>().ResetPosition();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
